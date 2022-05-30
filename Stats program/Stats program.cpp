685b5ac6f8d213c5dfb0a642fb8e1008a584ca06
Jack <10jack222@gmail.com>
2022-05-30 13:38:46 +1200

    Add project files.

---
+++ Stats program/Stats program.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+
+int const Num_of_trials = 1000;
+
+
+
+int main()
+{
+	for (int i = 0; i < Num_of_trials; i += 1) {
+		int random_num_1 = rand() % 100 + 1;
+		int random_num_2 = rand() % 10 + 1;
+		if (random_num_1 == 1) {
+			if()
+		}
+	}
+   
+}
+
